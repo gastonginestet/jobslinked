96388ccbabea614116c2178799c9811e30fe860a
Gastón <gaston.ginestet@snappler.com>
2021-08-10 20:24:08 -0300

    routes,schema and seeds updated + job alerts and follow models added

---
+++ app/models/job_alert.rb
@@ -0,0 +1,6 @@
+class JobAlert < ApplicationRecord
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  belongs_to :user
+end
